f231b8c397ceb74fae8222d446384f4e298114fd
Matthew Rothenberg <mrothenberg@gmail.com>
2018-12-07 12:07:21 -0500

    test exercises cache validity

--- http_test.go
+++ http_test.go
@@ -9,6 +9,14 @@ import (
 	"github.com/pmylund/go-cache"
 )
 
+const testIPv4Path1 = "/?ip=89.160.20.112"
+const testIPv4Path2 = "/?ip=81.2.69.142"
+const testIPv6Path1 = "/?ip=2001:218:85a3:0000:0000:8a2e:0370:7334"
+
+const testIPv4Body1 = `{"country":{"iso_code":"SE"},"location":{"lat":58.4167,"long":15.6167,"accuracy":76}}`
+const testIPv4Body2 = `{"country":{"iso_code":"GB"},"location":{"lat":51.5142,"long":-0.0931,"accuracy":10}}`
+const testIPv6Body1 = `{"country":{"iso_code":"JP"},"location":{"lat":35.68536,"long":139.75309,"accuracy":100}}`
+
 func TestHTTPLookup(t *testing.T) {
 	var httpCases = []struct {
 		name           string
@@ -18,18 +26,34 @@ func TestHTTPLookup(t *testing.T) {
 		expectedBody   string
 	}{
 		{
-			name:           "happy IPv4",
-			path:           "/?ip=89.160.20.112",
+			name:           "happy1 IPv4",
+			path:           testIPv4Path1,
 			expectedStatus: http.StatusOK,
 			expectedType:   "application/json",
-			expectedBody:   `{"country":{"iso_code":"SE"},"location":{"lat":58.4167,"long":15.6167,"accuracy":76}}`,
+			expectedBody:   testIPv4Body1,
+		},
+		{
+			name:           "happy2 IPv4",
+			path:           testIPv4Path2,
+			expectedStatus: http.StatusOK,
+			expectedType:   "application/json",
+			expectedBody:   testIPv4Body2,
 		},
 		{
 			name:           "happy IPv6",
-			path:           "/?ip=2001:218:85a3:0000:0000:8a2e:0370:7334",
+			path:           testIPv6Path1,
 			expectedStatus: http.StatusOK,
 			expectedType:   "application/json",
-			expectedBody:   `{"country":{"iso_code":"JP"},"location":{"lat":35.68536,"long":139.75309,"accuracy":100}}`,
+			expectedBody:   testIPv6Body1,
+		},
+		{
+			// re-request the first valid path after other path requests, in
+			// order to make certain about exercising cache validity
+			name:           "happy1 IPv4 repeated",
+			path:           testIPv4Path1,
+			expectedStatus: http.StatusOK,
+			expectedType:   "application/json",
+			expectedBody:   testIPv4Body1,
 		},
 		{
 			name:           "request empty",
@@ -66,50 +90,62 @@ func TestHTTPLookup(t *testing.T) {
 		t.Fatal(err)
 	}
 	defer db.Close()
-	handler := HTTPHandler{
-		DB:       db,
-		MemCache: nil,
+
+	// test both with a cache and without
+	rawHandler := HTTPHandler{DB: db, MemCache: nil}
+	cachedHandler := HTTPHandler{DB: db, MemCache: cache.New(DefaultCacheExpiration, DefaultCacheCleanup)}
+	var handlerCases = []struct {
+		name    string
+		handler HTTPHandler
+	}{
+		{name: "raw", handler: rawHandler},
+		{name: "cached", handler: cachedHandler},
 	}
+	for _, hc := range handlerCases {
+		t.Run(hc.name, func(t *testing.T) {
 
-	for _, tc := range httpCases {
-		t.Run(tc.name, func(t *testing.T) {
-			req, err := http.NewRequest(http.MethodGet, tc.path, nil)
-			if err != nil {
-				t.Fatal(err)
+			for _, tc := range httpCases {
+				t.Run(tc.name, func(t *testing.T) {
+					req, err := http.NewRequest(http.MethodGet, tc.path, nil)
+					if err != nil {
+						t.Fatal(err)
+					}
+
+					rr := httptest.NewRecorder()
+					hc.handler.ServeHTTP(rr, req)
+
+					// check the status code is what we expect
+					if status := rr.Code; status != tc.expectedStatus {
+						t.Errorf("handler returned wrong status code: got %v want %v",
+							status, tc.expectedStatus)
+					}
+
+					// check content type is what we expect
+					if ct := rr.Header().Get("content-type"); ct != tc.expectedType {
+						t.Errorf("handler returned wrong content-type: got %v want %v",
+							ct, tc.expectedType)
+					}
+
+					// check the response body is valid json
+					if bytes := rr.Body.Bytes(); !json.Valid(bytes) {
+						t.Errorf("json response did not validate! %v", bytes)
+					}
+
+					// check the response body is what we expect
+					if body := rr.Body.String(); body != tc.expectedBody {
+						t.Errorf("handler returned unexpected body: got %v want %v",
+							body, tc.expectedBody)
+					}
+				})
 			}
 
-			rr := httptest.NewRecorder()
-			handler.ServeHTTP(rr, req)
-
-			// check the status code is what we expect
-			if status := rr.Code; status != tc.expectedStatus {
-				t.Errorf("handler returned wrong status code: got %v want %v",
-					status, tc.expectedStatus)
-			}
-
-			// check content type is what we expect
-			if ct := rr.Header().Get("content-type"); ct != tc.expectedType {
-				t.Errorf("handler returned wrong content-type: got %v want %v",
-					ct, tc.expectedType)
-			}
-
-			// check the response body is valid json
-			if bytes := rr.Body.Bytes(); !json.Valid(bytes) {
-				t.Errorf("json response did not validate! %v", bytes)
-			}
-
-			// check the response body is what we expect
-			if body := rr.Body.String(); body != tc.expectedBody {
-				t.Errorf("handler returned unexpected body: got %v want %v",
-					body, tc.expectedBody)
-			}
 		})
 	}
 }
 
-// TODO: test caching validity
-// idea: send req 1A,2B,3A.  verify 1==3, 1!=2, 2!=3
-
+// // Below is a leftover test struct I was using instead of httptest.ResponseRecorder,
+// // thinking that it would reduce perf overhead in benchmarking, but it didnt seem to
+// // make a big difference, so leaving out for now but preserving for future thoughts.
 // type NullResponseWriter struct{}
 
 // func (n NullResponseWriter) Header() http.Header {
