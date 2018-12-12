2c66431ee6d3a4904f3d0c79d9cc69211fc4769e
Matthew Rothenberg <mrothenberg@gmail.com>
2018-12-12 13:38:48 -0500

    allow explicit cache disable from CLI

--- cmd/geominder/main.go
+++ cmd/geominder/main.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	var dbPath = flag.String("db", "data/GeoLite2-City.mmdb", "Path of MaxMind GeoIP2/GeoLite2 database")
-	var cacheSize = flag.Uint("cache", geominder.DefaultMaxCacheSize, "Max memory used for caching responses in MB")
+	var cacheSize = flag.Uint("cache", geominder.DefaultMaxCacheSize, "Max memory used for cache in MB, 0 disables")
 	var originPolicy = flag.String("origin", geominder.DefaultOriginPolicy, `Value for 'Access-Control-Allow-Origin' header, set to "" to disable.`)
 	var port = flag.Int("port", 9000, "Port to listen for connections on")
 	var threads = flag.Int("threads", runtime.NumCPU(), "Number of threads to use, otherwise number of detected cores")
@@ -27,7 +27,12 @@ func main() {
 	}
 	defer db.Close()
 
-	lh := geominder.NewHTTPHandler(db).SetOriginPolicy(*originPolicy).EnableCacheOfSize(*cacheSize)
+	lh := geominder.NewHTTPHandler(db).SetOriginPolicy(*originPolicy)
+	if *cacheSize != 0 {
+		lh.EnableCacheOfSize(*cacheSize)
+	} else {
+		lh.DisableCache()
+	}
 
 	// Logging of connections is disabled
 	// Logging of connections is enabled, this may severely impact performance under extremely high utilization
