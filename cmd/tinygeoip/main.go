c53bc3f56fd4ae5a139257771331afe7a7620133
Matthew Rothenberg <mrothenberg@gmail.com>
2018-12-24 11:14:50 -0500

    CLI prompt for addr instead of port
    This idiom may be more confusing for users who are not familiar with
    Go's "addr" notation (e.g. "127.0.0.1:9000", ":9000", etc) but allows
    for more flexibility in specifying what specific interface to bind on,
    which is important in many server environments.

--- cmd/tinygeoip/main.go
+++ cmd/tinygeoip/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
 	"runtime"
@@ -13,7 +12,7 @@ import (
 func main() {
 	var dbPath = flag.String("db", "data/GeoLite2-City.mmdb", "Path for MaxMind database file")
 	var originPolicy = flag.String("origin", tinygeoip.DefaultOriginPolicy, `'Access-Control-Allow-Origin' header, empty disables`)
-	var port = flag.Int("port", 9000, "Port to listen for connections on")
+	var addr = flag.String("addr", ":9000", "Address to listen for connections on")
 	var threads = flag.Int("threads", runtime.NumCPU(), "Number of threads to use, otherwise number of CPUs")
 	// var verbose = flag.Bool("verbose", false, "log all requests")
 
@@ -36,8 +35,8 @@ func main() {
 	// Logging of connections is enabled, this may severely impact performance under extremely high utilization
 
 	http.Handle("/", lh)
-	log.Println("Listening for connections on port", *port)
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil); err != nil {
+	log.Println("Listening for connections on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
